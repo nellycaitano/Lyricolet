c619ca5726541c7456068ca8776c99bb41cccc24
KarstenPH <66064356+KarstenPH@users.noreply.github.com>
2023-03-09 11:43:36 +0400

    Change some stuff up

--- SN001DEMO.js
+++ SN001DEMO.js
@@ -1,53 +1,53 @@
 >
 <!--Saturday Nights | DEMO TEST FILE-->
-      // Set the video ID
-      var videoID = "https://youtu.be/aT3s4v2poMY";
+var videoID = "aT3s4v2poMY";
 
-      // Set the start time of the video
-      var startTime = 0;
+var startTime = 0;
 
-      // Set the lyrics
-      var lyrics = `[00:20.00]First line of lyrics
+var lyrics = `[00:20.00]First line of lyrics
 [00:30.00]Second line of lyrics
 [00:40.00]Third line of lyrics`;
 
-      // Create a new YouTube player object
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '360',
-          width: '640',
-          videoId: videoID,
-          startSeconds: startTime,
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
+var player;
 
-      // Display the lyrics
-      var lyricsDiv = document.getElementById("lyrics");
-      var lyricLines = lyrics.split("\n");
-      var currentLine = 0;
-      var lastLineTime = -1;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING) {
-          setInterval(function() {
-            var currentTime = player.getCurrentTime();
-            var currentLineText = lyricLines[currentLine];
-            var matches = currentLineText.match(/\[(\d+):(\d+\.\d+)\](.*)/);
-            if (matches && matches.length == 4) {
-              var lineTime = parseFloat(matches[1]) * 60 + parseFloat(matches[2]);
-              var lineText = matches[3];
-              if (lineTime > lastLineTime && currentTime >= lineTime) {
-                lyricsDiv.scrollTop = lyricsDiv.scrollHeight;
-                lyricsDiv.innerHTML += "<br/><strong>" + lineText + "</strong>";
-                currentLine++;
-                lastLineTime = lineTime;
-              }
-            }
-          }, 100);
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId: videoID,
+    startSeconds: startTime,
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+var lyricsDiv = document.getElementById("lyrics");
+var lyricLines = lyrics.split("\n");
+var currentLine = 0;
+var lastLineTime = -1;
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING)
+    setInterval(function() {
+      var currentTime = player.getCurrentTime();
+      var currentLineText = lyricLines[currentLine];
+      var matches = currentLineText.match(/\[(\d+):(\d+\.\d+)\]/);
+      if (matches) {
+        var lineTime = parseInt(matches[1]) * 60 + parseFloat(matches[2]);
+        if (currentTime >= lineTime) {
+          if (lastLineTime == -1 || currentTime >= lastLineTime + 1) {
+            lyricsDiv.innerHTML = currentLineText.replace(matches[0], "");
+            lastLineTime = currentTime;
+            currentLine++;
+          }
         }
       }
-    </script>
+    }, 100);
+  }
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
