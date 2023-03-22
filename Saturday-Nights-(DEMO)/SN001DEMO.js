d0c7e5b4e673c4069fd4d8ff99c5b12c2d318433
KarstenPH <66064356+KarstenPH@users.noreply.github.com>
2023-03-22 14:39:58 +0400

    Update SN001DEMO.js

--- Saturday-Nights-(DEMO)/SN001DEMO.js
+++ Saturday-Nights-(DEMO)/SN001DEMO.js
@@ -4,9 +4,59 @@ const lyrics = `[00:20.00]First line of lyrics
 [00:30.00]Second line of lyrics
 [00:40.00]Third line of lyrics`;
 
-let player;
-let currentLine = 0;
-let lastLineTime = -1;
+let player, currentLine = 0, lastLineTime = -1;
+const lyricsDiv = document.getElementById("lyrics");
+
+const onPlayerStateChange = event => {
+  if (event.data === YT.PlayerState.PLAYING) {
+    setInterval(() => {
+      const currentTime = player.getCurrentTime();
+      const currentLineText = lyricLines[currentLine];
+      const matches = currentLineText.match(/\[(\d+):(\d+\.\d+)\]/);
+      if (matches) {
+        const lineTime = parseInt(matches[1]) * 60 + parseFloat(matches[2]);
+        if (currentTime >= lineTime) {
+          if (lastLineTime === -1 || currentTime >= lastLineTime + 1) {
+            lyricsDiv.childNodes[currentLine].classList.remove("current");
+            currentLine++;
+            lyricsDiv.childNodes[currentLine].classList.add("current");
+            lastLineTime = currentTime;
+          }
+        }
+      }
+    }, 100);
+  }
+};
+
+const onPlayerReady = event => event.target.playVideo();
+
+const onPlayerError = event => {
+  const errorCode = event.data;
+  if ([2, 100, 101, 150].includes(errorCode)) {
+    console.error(`YouTube video error. Error code: ${errorCode}`);
+    document.getElementById("player").innerHTML = "There was an error loading the YouTube video.";
+  }
+};
+
+const copyLyrics = () => {
+  const lyricsText = lyricsDiv.innerText;
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(lyricsText).then(() => {
+      alert("Lyrics copied to clipboard!");
+    }).catch(() => {
+      copyFallback(lyricsText);
+    });
+  }
+};
+
+document.getElementById("copyButton").addEventListener("click", copyLyrics);
+
+const lyricLines = lyrics.split("\n");
+lyricLines.forEach(line => {
+  const lineDiv = document.createElement("div");
+  lineDiv.innerText = line.replace(/\[\d\d:\d\d\.\d+\]/, "");
+  lyricsDiv.appendChild(lineDiv);
+});
 
 const playerReadyCheck = setInterval(() => {
   if (typeof YT !== "undefined" && YT.loaded) {
@@ -28,44 +78,3 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
-
-function onPlayerError(event) {
-  const errorCode = event.data;
-  if ([2, 100, 101, 150].includes(errorCode)) {
-    console.error(`YouTube video error. Error code: ${errorCode}`);
-    document.getElementById("player").innerHTML = "There was an error loading the YouTube video.";
-  }
-}
-
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.PLAYING) {
-    setInterval(() => {
-      const currentTime = player.getCurrentTime();
-      const currentLineText = lyricLines[currentLine];
-      const matches = currentLineText.match(/\[(\d+):(\d+\.\d+)\]/);
-      if (matches) {
-        const lineTime = parseInt(matches[1]) * 60 + parseFloat(matches[2]);
-        if (currentTime >= lineTime) {
-          if (lastLineTime === -1 || currentTime >= lastLineTime + 1) {
-            lyricsDiv.childNodes[currentLine].classList.remove("current");
-            currentLine++;
-            lyricsDiv.childNodes[currentLine].classList.add("current");
-            lastLineTime = currentTime;
-          }
-        }
-      }
-    }, 100);
-  }
-}
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-const lyricsDiv = document.getElementById("lyrics");
-const lyricLines = lyrics.split("\n");
-
-for (let i = 0; i < lyricLines.length; i++) {
-  const line = lyricLines[i];
-  const lineDiv = document.createElement("div");
-  lineDiv.textContent = line.replace(/\[\
